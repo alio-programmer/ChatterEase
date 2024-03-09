e708c4a935574ca4c5ac8b4a117523fc4dc04d44
Gaurav singh bisht <gauravbisht82754@gmail.com>
2024-03-09 15:07:35 +0530

    UI part completed

--- frontend/chatappfront/src/Components/MessageContainer/MessageContainer.jsx
+++ frontend/chatappfront/src/Components/MessageContainer/MessageContainer.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import Messages from "./Messages";
 import Messageinput from "./Messageinput";
+import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
+  const noChatSelected = true;
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      <>
-        <div className="bg-slate-500 px-4 py-2 mb-2 rounded-lg">
-          <span className="label-text">To: </span>
-          <span className="text-gray-900 font-bold">John Doe</span>
-        </div>
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          <div className="bg-slate-500 px-4 py-2 mb-2 rounded-lg">
+            <span className="label-text">To: </span>
+            <span className="text-gray-900 font-bold">John Doe</span>
+          </div>
 
-        <Messages />
-        <Messageinput />
-      </>
+          <Messages />
+          <Messageinput />
+        </>
+      )}
     </div>
   );
 };
 
 export default MessageContainer;
+
+const NoChatSelected = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <p>Welcome 👋 John Doe</p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
+      </div>
+    </div>
+  );
+};
