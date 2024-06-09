bfa42364d580019005e4be72fbb3d5527df1e6e1
Gaurav singh bisht <gauravbisht82754@gmail.com>
2024-06-09 20:57:08 +0530

    deployment error v1.0

--- frontend/chatappfront/src/Context/SocketContext.jsx
+++ frontend/chatappfront/src/Context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (AuthUser) {
       const socket = io(
-        "http://localhost:8000",
+        "http://localhost:8000/",
         {
           query: {
             userId: AuthUser._id,
