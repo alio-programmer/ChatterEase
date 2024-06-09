351e015a44afe187e4f4826ae6300aad0895e9a6
Gaurav singh bisht <gauravbisht82754@gmail.com>
2024-06-09 14:38:32 +0530

    final build

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
 const messageRoutes = require("./routes/message.routes");
@@ -12,11 +13,18 @@ app.use(cors());
 app.use(express.json()); //to parse the incoming request with json payloads (from req.body)
 dotenv.config();
 app.use(cookieParser());
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/chatappfront/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/chatappfront/dist/index.html")
+  );
+});
 
 server.listen(PORT, () => {
   console.log(`server running on port http://localhost:${PORT}`);
